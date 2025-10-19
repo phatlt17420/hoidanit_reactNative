de61a272f6f3b95b7ffd6884c3048d264bd28d3e
phatlt17420 <phatlt17420@gmail.com>
2025-10-20 00:14:07 +0700

    video6

--- App.tsx
+++ App.tsx
@@ -1,14 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+
+  const [count, setCount] = useState(0);
+  const [name, setName] = useState("");
+  const [age, setAge] = useState<number>(0);
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text>Name: {name}</Text>
+        <TextInput
+          multiline
+          style={{
+            borderColor: 'green',
+            borderWidth: 2,
+            width: 200,
+            padding: 15
+          }}
+          onChangeText={(value) => { setName(value) }}
+        ></TextInput>
+      </View>
+      <View>
+        <Text>Age: {age}</Text>
+        <TextInput
+          multiline
+          style={{
+            borderColor: 'green',
+            borderWidth: 2,
+            width: 200,
+            padding: 15
+          }}
+          onChangeText={(value) => { setAge(+value) }}
+          keyboardType='numeric'
+          maxLength={2}
+        ></TextInput>
+      </View>
+      <Text style={styles.hello1}>Count = {count}</Text>
+      <View>
+        <Button
+          title='Increase' onPress={() => setCount(count + 1)} />
+      </View>
     </View>
   );
 }
+
+
+// css in js// không có khái niệm CSS
 
 const styles = StyleSheet.create({
   container: {
@@ -17,4 +58,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  hello1:
+  {
+    color: "red",
+    fontSize: 40,
+    borderColor: "green",
+    borderWidth: 5,
+    padding: 10
+  }
+
 });
